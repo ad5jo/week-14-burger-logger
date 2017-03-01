c25118e7dde709f3b1970c71e725f4021b4de6c8
Dave Durkee <ad5jo@yahoo.com>
2017-03-01 15:54:11 -0600

    finished

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE burgers
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	date TIMESTAMP DEFAULT NOW(),
+	date TIMESTAMP NOT NULL DEFAULT NOW(),
 	PRIMARY KEY (id)
 );
