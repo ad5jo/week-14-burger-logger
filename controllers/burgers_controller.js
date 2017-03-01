5076e1052dcebee6bfe589fa1233880c27230bd0
Dave Durkee <ad5jo@yahoo.com>
2017-03-01 15:43:50 -0600

    fixed the Add Burger Button

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,25 +5,48 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
+
+
+
+
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
+    console.log("burgers_controller - get - select all");
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
+
+// INSERT INTO burgers (burger_name, devoured, date) VALUES ('South Park Slider', false, '2017-02-01 12:13:14');
 router.post("/", function(req, res) {
-  burger.insertOne([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name, req.body.devoured
-  ], function() {
-    res.redirect("/");
+
+  var the_burger = "\"the king\"";
+  var the_state = "false" ;
+  the_burger = "\"the king\"";
+  the_burger = "\""+ req.body.burger_name + "\"";
+  if (the_burger === "\"\"")
+  {
+    console.log("no name entered");
+    return;
+  }
+  the_state = "false" ;
+  
+  console.log("burgers_controller -the_burger: " + the_burger);
+  console.log("burgers_controller - the_state: " + the_state);
+
+  burger.insertOne
+  ("burger_name", "devoured", 
+    the_burger,the_state ,
+      function() {
+      console.log("burgers_controller - get - insert one");
+      res.redirect("/");
   });
+
 });
 
 router.put("/:id", function(req, res) {
@@ -33,15 +56,19 @@ router.put("/:id", function(req, res) {
 
   burger.updateOne({
     devoured: req.body.devoured
-  }, condition, function() {
+  }, condition, 
+  function() {
+    console.log("burgers_controller - get - update one");
     res.redirect("/");
   });
 });
+
 
 router.delete("/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   burger.delete(condition, function() {
+    console.log("burgers_controller - get - delete");
     res.redirect("/");
   });
 });
--- models/burger.js
+++ models/burger.js
@@ -3,22 +3,30 @@ var orm = require("../config/orm.js");
 
 var burger = {
   selectAll: function(cb) {
+    console.log("burger - select all");
     orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
+
+
   // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
+  insertOne: function(nn,cc,vn,vc , cb) {
+    console.log("burger - inserts one");
+    orm.insertOne("burgers", nn,cc,vn,vc , function(res) {
       cb(res);
     });
   },
+
   updateOne: function(objColVals, condition, cb) {
+    console.log("burger - update one");
     orm.updateOne("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
+
   delete: function(condition, cb) {
+    console.log("burger - delete");
     orm.delete("burgers", condition, function(res) {
       cb(res);
     });
